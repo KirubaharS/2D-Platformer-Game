4b3747b6f622b3a15ec3b6b217bcab349dad6814
Kirubahar S <140052838+KirubaharS@users.noreply.github.com>
2024-04-22 15:34:45 +0530

    created animation controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        Debug.Log("Speed: " + speed);
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        Vector3 scale = transform.localScale;
+        if (speed < 0f)
+        {
+            scale.x = -1f* Mathf.Abs(scale.x);
+        } else if (speed > 0f)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+}
