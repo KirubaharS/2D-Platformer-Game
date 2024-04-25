cd457453125cced141a2512ce1fc9b7a3de6af0e
Kirubahar S <140052838+KirubaharS@users.noreply.github.com>
2024-04-25 15:22:50 +0530

    created level Restart

---
+++ Assets/Scripts/LevelRestart.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelRestart : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Player Level Restart");
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
